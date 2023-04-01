bec328f65c99b9ec4b1fda6b4a5b63b253ebd437
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-04-02 02:26:38 +0600

    Header Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, Routes, Route } from 'react-router-dom';
 import * as Page from './pages';
 import * as Comp from './components';
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Page.UserList />,
-      errorElement: <Page.Error />,
-    },
-    {
-      path: '/user/:id',
-      element: <Page.UserProfile />,
-      errorElement: <Page.Error />,
-    },
-  ]);
-
   return (
     <>
       <section className=" container">
         <Comp.Header />
-        <RouterProvider router={router} />
+
+        <Routes>
+          <Route
+            path="/"
+            element={<Page.UserList />}
+            errorElement={<Page.Error />}
+          />
+
+          <Route
+            path="/user/:id"
+            element={<Page.UserProfile />}
+            errorElement={<Page.Error />}
+          />
+        </Routes>
       </section>
       <Comp.Footer />
     </>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
   return (
     <nav className=" py-4 text-center lg:text-left">
-      <h2 className=" font-bold text-3xl">React Dev Coding Assessment</h2>
+      <h3 className=" font-bold text-3xl cursor-pointer">
+        <Link to="/">React Dev Coding Assessment</Link>
+      </h3>
+
       <span className=" font-thin text-base">By Code Rex</span>
     </nav>
   );
